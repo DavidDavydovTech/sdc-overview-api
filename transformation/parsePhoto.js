ab9d68b6edc35ce03021d486002c2586f0f248a1
David J. Davydov <davydovjd611@gmail.com>
2020-10-23 12:09:26 -0400

    New commit

--- transformation/parsePhoto.js
+++ transformation/parsePhoto.js
@@ -4,11 +4,11 @@ const path = require('path');
 const Transform = require('stream').Transform;
 
 let leftover = null;
-const filePath = path.join(__dirname, 'sku.csv')
+const filePath = path.join(__dirname, 'photo.csv')
 try {
     const read = fs.createReadStream(filePath);
     read.setEncoding('UTF8'); // Set the encoding to be utf8. 
-
+    console.log('Started...')
     // Handle stream events --> data, end, and error
     read.on('data', function(chunk) {
         // const values = chunk.split(/\n(?=(?:[^"\\]*(?:\\.|"(?:[^"\\]*\\.)*[^"\\]*"))*[^"]*$)/);
@@ -36,36 +36,37 @@ try {
 
             switch(true) {
                 case arr.length !== 4:
-                    // console.log(`[${i}] Warning, a line was incomplete!`, arr);
+                    //console.log(`[${i}] Warning, a line was incomplete!`, arr);
                     continue;
-                // case isNaN(Number.parseInt(arr[0])):
-                    // console.log(`[${i}] Warning, id was mutated (...but do we care?)`, arr[0]);
                 case isNaN(Number.parseInt(arr[1])):
-                    console.log(`[${i}] Warning, style_id was mutated!`, arr[1]);
+                    //console.log(`[${i}] Warning, style_id was mutated!`, arr[1]);
                     continue;
-                case arr[2][0] !== "\"" || arr[2][arr[2].length - 1] !== "\"":
-                    console.log(`[${i}] Warning, size was mutated!`, arr[2]);
+                case 
+                (
+                    arr[2][0] !== "\"" 
+                    || arr[2][arr[2].length - 1] !== "\""
+                )
+                && (arr[2].match(/"/g) || []).length % 2 === 0:
+                    //console.log(`[${i}] Warning, size was mutated!`, arr[2]);
                     continue;
-                case isNaN(Number.parseInt(arr[3])):
-                    // console.log(`[${i}] Warning, id was mutated (...but do we care?)`, arr[3]);
-                    nullAmount = true;
+                case 
+                (
+                    arr[3][0] !== "\"" 
+                    || arr[3][arr[3].length - 1] !== "\""
+                )
+                && (arr[3].match(/"/g) || []).length % 2 === 0:
+                    continue;
                 default:
                     // no errors!
             }
 
             if (!abort) {
-                const pathToFile = path.join(__dirname, 'sku', `${arr[1]}.json`);
-                // let exists = fs.existsSync(pathToFile);
-                // if (!exists) {
-                //     fs.openSync(pathToFile, 'w');
-                // }
-
-                fs.appendFileSync(
-                    pathToFile, 
-                    `{"size":${arr[2]},"amount":${nullAmount?'null':arr[3]}}\n`, 
+                fs.appendFile(
+                    path.join(__dirname, 'photo', `${arr[1]}.json`), 
+                    `{"image":${arr[2]},"thumbnail":${arr[3]}}\n`, 
                     (err) => {
                         if (err) console.log(err);
-                    },
+                    }
                 );
             }
         }
